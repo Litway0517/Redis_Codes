e5a551c05b1ec54f95d6e08d71ec03c1cd439744
Litway_ <xmw0517@gmail.com>
2022-08-06 16:45:44 +0800

    hmdp|点评笔记  还原

--- hm-dianping/src/main/java/com/hmdp/controller/BlogController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/BlogController.java
@@ -34,14 +34,15 @@ public class BlogController {
 
     @PostMapping
     public Result saveBlog(@RequestBody Blog blog) {
-        /*// 获取登录用户
+        // 获取登录用户
         UserDTO user = UserHolder.getUser();
         blog.setUserId(user.getId());
         // 保存探店博文
         blogService.save(blog);
         // 返回id
-        return Result.ok(blog.getId());*/
-        return Result.fail("未知错误");
+        return Result.ok(blog.getId());
+
+        // return Result.fail("未知错误");
     }
 
     @PutMapping("/like/{id}")
@@ -54,15 +55,16 @@ public class BlogController {
 
     @GetMapping("/of/me")
     public Result queryMyBlog(@RequestParam(value = "current", defaultValue = "1") Integer current) {
-        /*// 获取登录用户
+        // 获取登录用户
         UserDTO user = UserHolder.getUser();
         // 根据用户查询
         Page<Blog> page = blogService.query()
                 .eq("user_id", user.getId()).page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
         // 获取当前页数据
         List<Blog> records = page.getRecords();
-        return Result.ok(records);*/
-        return Result.fail("未知错误");
+        return Result.ok(records);
+
+        // return Result.fail("未知错误");
     }
 
     @GetMapping("/hot")
