731c0b35f49307fd3d110a7ad1d36ae1f0ca54ce
Litway_ <xmw0517@gmail.com>
2022-08-06 11:57:17 +0800

    hmdp  原来的注释错了

--- hm-dianping/src/main/java/com/hmdp/utils/RegexUtils.java
+++ hm-dianping/src/main/java/com/hmdp/utils/RegexUtils.java
@@ -9,7 +9,7 @@ public class RegexUtils {
     /**
      * 是否是无效手机格式
      * @param phone 要校验的手机号
-     * @return true:符合，false：不符合
+     * @return false:符合  true:不符合
      */
     public static boolean isPhoneInvalid(String phone){
         return mismatch(phone, RegexPatterns.PHONE_REGEX);
