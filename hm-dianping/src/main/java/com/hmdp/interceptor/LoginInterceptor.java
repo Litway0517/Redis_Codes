9ecba9fd35190787f59ae7ca55005298da6a1473
Litway_ <xmw0517@gmail.com>
2022-08-08 15:12:24 +0800

    hmdp|登录  登录拦截器preHandle方法 从请求头中获取authorization字段 若为空说明为登录 直接拦截 若有值再去redis查询 若为空则未登录 拦截 如果成功从redis中取到用户信息 那么将map对象转换为UserDTO对象存到LocalThread中 更新过期时间并放行请求 注意LoginInterceptor拦截器在注入RedisTemplate时是通过的构造方法注入的 因为LoginInterceptor并没有交由Spring容器管理里

--- hm-dianping/src/main/java/com/hmdp/interceptor/LoginInterceptor.java
+++ hm-dianping/src/main/java/com/hmdp/interceptor/LoginInterceptor.java
@@ -1,42 +1,85 @@
 package com.hmdp.interceptor;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
 import com.hmdp.dto.UserDTO;
+import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.constant.UserConstant.AUTHORIZATION;
+
+
+
+/*
+    TODO:
+        - 这个LoginInterceptor拦截器并不是由spring容器管理的, 也就是说初始化的时候是手动new出来的, 就在SpringMvcConfig中
+        - 因此为了在LoginInterceptor中使用redisTemplate, 应该使用构造方法
+        SpringMvcConfig这个类是由spring容器进行管理创建的, 因此可以在这个配置类自动注入redisTemplate, 然后再赋值给LoginInterceptor.
+
+        不使用以上方法的话, 就在LoginInterceptor加上@Component注解
+ */
 
 public class LoginInterceptor implements HandlerInterceptor {
 
+    private final StringRedisTemplate redisTemplate;
+
+    public LoginInterceptor(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 1- 获取session
-        HttpSession session = request.getSession();
-
-        // 2- 获取session中的用户
-        Object userDTO = session.getAttribute("user");
-
-        // 3- 判断
-        if (userDTO == null) {
-            // 4- 不存在 拦截 返回状态码401 未授权
+        // TODO: 1- 获取session -> 改为获取请求头中的token
+        String token = request.getHeader(AUTHORIZATION);
+        if (StrUtil.isBlank(token)) {
+            // 用户未登录 直接拦截. 返回状态码401 未授权.
             response.setStatus(401);
             return false;
         }
 
-        // 5- 存在 保存到ThreadLocal 有工具类
+        // TODO: 2- 获取session中的用户 -> 改为基于token获取redis中的用户信息
+        String loginUserKey = RedisConstants.LOGIN_USER_KEY + token;
+        Map<Object, Object> userMap = redisTemplate.opsForHash().entries(loginUserKey);
 
-        UserHolder.saveUser((UserDTO) userDTO);
+        // TODO: 3- 判断用户是否存在 即是否已经登录
+        if (userMap.isEmpty()) {
+            // 未登录 直接拦截. 返回状态码401 未授权
+            response.setStatus(401);
+            return false;
+        }
+
+        // TODO: 5- 将 从redis中取出来的用户转为UserDTO对象
+        UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
+
+        /*
+            TODO: 6- 存在 保存到ThreadLocal 有工具类.
+            注意这里仍然是保存到ThreadLocal的内存中 并不是redis 因为这个请求会被分发到不同的tomcat
+         */
+        UserHolder.saveUser(userDTO);
+
+        /*
+            TODO: 7- 刷新token的有效期 即用户登录状态的有效期
+            只有用户连续30分钟不操作才会在redis中清楚这个用户 这也是session的做法
+         */
+        redisTemplate.expire(loginUserKey, RedisConstants.LOGIN_USER_TTL, TimeUnit.MINUTES);
+
         return true;
     }
 
     /**
      * 拦截器的这个方法是在线程处理完之后执行的
-     * @param request 请求
+     *
+     * @param request  请求
      * @param response 响应
-     * @param handler 处理
-     * @param ex 异常
+     * @param handler  处理
+     * @param ex       异常
      * @throws Exception 异常
      */
     @Override
