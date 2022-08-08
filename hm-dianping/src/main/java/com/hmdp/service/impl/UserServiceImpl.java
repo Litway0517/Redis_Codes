47e6041c5f50132e3be9631d334df126b9d50eea
Litway_ <xmw0517@gmail.com>
2022-08-08 12:33:14 +0800

    hmdp|登录  修改登录login的逻辑 校验数据 转换数据 脱敏 保存用户到redis

--- hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hmdp.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
@@ -17,10 +18,13 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.LOGIN_CODE_KEY;
 import static com.hmdp.utils.RedisConstants.LOGIN_CODE_TTL;
+import static com.hmdp.utils.RedisConstants.LOGIN_USER_KEY;
+import static com.hmdp.utils.RedisConstants.LOGIN_USER_TTL;
 import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
@@ -93,36 +97,39 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             return Result.fail("手机号格式错误，请仔细检查！");
         }
 
-        // 2- 校验验证码
-        // 从redis中获取验证码
+        // 2- 校验验证码 -> 从redis中获取验证码
         String cacheCode = redisTemplate.opsForValue().get(LOGIN_CODE_KEY + phone);
         String code = loginForm.getCode();
         if (cacheCode == null) {
-            return Result.fail("验证码已经超时");
+            return Result.fail("验证码已经超时，请重新发送！");
         }
-        if (cacheCode == null || !code.equals(cacheCode)) {
+        if (!code.equals(cacheCode)) {
             // 不一致
             return Result.fail("验证码错误！");
         }
 
-        // 4- 一致
+        // 4- 一致 根据手机号查询用户
         User user = query().eq("phone", phone).one();
 
         // 5- 根据手机号查询用户
-        if (user==null) {
+        if (user == null) {
             // 6- 用户不存在 那么就创建并保存
             user = createUserWithPhone(phone);
         }
 
         // TODO: 7- 保存用户信息到redis中  注意存储的时候不存储完整的User信息 必须脱敏处理 不然就会有密码等信息
+        // 7.1 随机生成token, 作为登陆令牌
+        String token = UUID.randomUUID().toString(true);
 
-        // TODO: 7.1 随机生成token, 作为登陆令牌
+        // 7.2 将User对象转换为UserDTO再转换为HashMap存储
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        Map<String, Object> userMap = BeanUtil.beanToMap(userDTO);
 
-        // TODO: 7.2 将User对象转换为Hash存储
-
-        // TODO: 7.3 存储
-
-        session.setAttribute("user", BeanUtil.copyProperties(user, UserDTO.class));
+        // 7.3 存储 -> 改为存储到redis key的结构是token value的结构是hash
+        String tokenKey = LOGIN_USER_KEY + token;
+        redisTemplate.opsForHash().putAll(tokenKey, userMap);
+        // 设置有效期
+        redisTemplate.expire(tokenKey, LOGIN_USER_TTL, TimeUnit.MINUTES);
 
         // 返回token
         return Result.ok();
