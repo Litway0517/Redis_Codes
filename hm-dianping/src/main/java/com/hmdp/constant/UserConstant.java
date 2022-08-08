7efb8a4a0bffd0d3dc395cf8489dd61f488c48c1
Litway_ <xmw0517@gmail.com>
2022-08-08 11:40:04 +0800

    hmdp|登录  和用户有关的一些常量 后面改成使用redis常量了

---
+++ hm-dianping/src/main/java/com/hmdp/constant/UserConstant.java
@@ -0,0 +1,14 @@
+package com.hmdp.constant;
+
+public class UserConstant {
+
+    // 用户发送验证码时 服务器将验证码存储到redis中 用到的key的前缀
+    public static final String USER_LOGIN_SEND_CODE_KEY = "Login:Code:";
+
+    // 用户发送验证码的超时时间
+    public static final int USER_LOGIN_CODE_TIMEOUT = 2;
+
+
+
+
+}
