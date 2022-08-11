45ee8484190363171fdc19919a9dad08367e68f1
Litway_ <xmw0517@gmail.com>
2022-08-11 22:05:04 +0800

    hmdp|商户类型  将ShopType全部缓存到redis中 因为这一部分数据不经常改变 所以将其缓存起来加快查询速度

--- hm-dianping/src/main/java/com/hmdp/constant/ShopConstant.java
+++ hm-dianping/src/main/java/com/hmdp/constant/ShopConstant.java
@@ -4,6 +4,7 @@ public class ShopConstant {
     // 店铺存储到redis时的key前缀
     public static final String CACHE_SHOP_KEY = "Cache:Shop:Key:";
 
-
+    // 店铺类别列表 存储到redis时的key前缀
+    public static final String CACHE_SHOP_TYPE_KEY = "Cache:Shop:Type:key";
 
 }
