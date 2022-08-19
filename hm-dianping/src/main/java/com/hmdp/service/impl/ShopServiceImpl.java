6fa630614f2e85f3c007be2efae5a95edf2ebeea
Litway_ <xmw0517@gmail.com>
2022-08-19 22:12:14 +0800

    hmdp|商户信息  通过redis的setnx命令设计互斥锁 这样只有第一个线程能够成功获取锁 这样就能够使其他线程休眠尝试

--- hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -45,33 +45,14 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
      */
     @Override
     public Result queryById(Long id) {
-        // 1- 从redis中查询商户信息
-        String shopJson = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
+        // 缓存穿透
+        // Shop shop = queryWithPassThrough(id);
 
-        // 2- 判断是否存在
-        if (StrUtil.isNotBlank(shopJson)) {
-            // 3- 存在直接返回
-            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-            return Result.ok(shop);
-        }
-
-        // 因为向redis中存储的可能是空值 也可能是 店铺真实值 所以这里再判断一次
-        if (shopJson != null) {
-            return Result.ok("店铺信息不存在！");
-        }
-
-        // 4- 不存在 根据id查询数据库
-        Shop shop = getById(id);
-
-        // 5- 不存在数据库中
+        // 互斥锁解决缓存击穿问题
+        Shop shop = queryWithPMutex(id);
         if (shop == null) {
-            // 将空值存储到redis中 空字符串 过期时间改成2分钟 不应该设置得太长
-            stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
             return Result.fail("店铺不存在！");
         }
-
-        // 6- 存在数据库中 保存到redis中 -> 先转成JSON字符串 -> 过期时间设置为30分钟
-        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
 
         // 7- 返回
         return Result.ok(shop);
@@ -95,6 +76,108 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
      */
     private void unlock(String key) {
         stringRedisTemplate.delete(key);
+    }
+
+    /**
+     * 缓存穿透解决方法
+     * @param id id
+     * @return 结果
+     */
+    public Shop queryWithPassThrough(Long id) {
+        // 1- 从redis中查询商户信息
+        String shopJson = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
+
+        // 2- 判断是否存在
+        if (StrUtil.isNotBlank(shopJson)) {
+            // 3- 存在直接返回
+            return JSONUtil.toBean(shopJson, Shop.class);
+
+        }
+
+        // 因为向redis中存储的可能是空值 也可能是 店铺真实值 所以这里再判断一次
+        if (shopJson != null) {
+            return null;
+        }
+
+        // 4- 不存在 根据id查询数据库
+        Shop shop = getById(id);
+
+        // 5- 不存在数据库中
+        if (shop == null) {
+            // 将空值存储到redis中 空字符串 过期时间改成2分钟 不应该设置得太长
+            stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+            return null;
+        }
+
+        // 6- 存在数据库中 保存到redis中 -> 先转成JSON字符串 -> 过期时间设置为30分钟
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
+
+        // 7- 返回
+        return shop;
+    }
+
+
+    /**
+     * 互斥锁
+     * @param id id
+     * @return 结果
+     */
+    public Shop queryWithPMutex(Long id) {
+        // 1- 从redis中查询商户信息
+        String shopJson = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
+
+        // 2- 判断是否存在
+        if (StrUtil.isNotBlank(shopJson)) {
+            // 3- 存在直接返回
+            return JSONUtil.toBean(shopJson, Shop.class);
+
+        }
+
+        // 因为向redis中存储的可能是空值 也可能是 店铺真实值 所以这里再判断一次
+        if (shopJson != null) {
+            return null;
+        }
+
+        // 4- 实现缓存重建
+        // 4.1- 获取互斥锁
+        String lockKey = String.valueOf(LOCK_SHOP_TTL + id);
+        Shop shop = null;
+        try {
+            boolean isLock = tryLock(lockKey);
+            // 4.2- 判断是否获取成功
+            if (!isLock) {
+                // 4.3- 失败 则休眠重试 暂时尝试休眠50ms
+                Thread.sleep(50);
+                // 失败之后过了休眠还要重新尝试 因此就是递归
+                return queryWithPMutex(id);
+            }
+
+            // 4.4- 成功 根据id查询数据库
+            // 4- 不存在 根据id查询数据库
+            shop = getById(id);
+
+            // 模拟重建延时 让大量请求进入测试锁的可靠性
+            Thread.sleep(200);
+
+            // 5- 不存在数据库中
+            if (shop == null) {
+                // 将空值存储到redis中 空字符串 过期时间改成2分钟 不应该设置得太长
+                stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+                return null;
+            }
+
+            // 6- 存在数据库中 保存到redis中 -> 先转成JSON字符串 -> 过期时间设置为30分钟
+            stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // 7- 释放锁 无论上面成不成功都需要释放锁
+            unlock(lockKey);
+        }
+
+        // 8- 返回
+        return shop;
     }
 
 
