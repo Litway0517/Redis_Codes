b503256ed873313c252b23d1d63f3bc7cc0c8799
Litway_ <xmw0517@gmail.com>
2022-08-19 21:46:13 +0800

    hmdp|商户信息  redis常量

--- hm-dianping/src/main/java/com/hmdp/utils/RedisConstants.java
+++ hm-dianping/src/main/java/com/hmdp/utils/RedisConstants.java
@@ -9,9 +9,9 @@ public class RedisConstants {
 
     public static final Long CACHE_NULL_TTL = 2L;
     public static final Long CACHE_SHOP_TTL = 30L;
-    public static final String CACHE_SHOP_KEY = "cache:shop:";
+    public static final String CACHE_SHOP_KEY = "Cache:Shop:";
 
-    public static final String LOCK_SHOP_KEY = "lock:shop:";
+    public static final String LOCK_SHOP_KEY = "Lock:Shop:";
     public static final Long LOCK_SHOP_TTL = 10L;
 
     public static final String SECKILL_STOCK_KEY = "seckill:stock:";
