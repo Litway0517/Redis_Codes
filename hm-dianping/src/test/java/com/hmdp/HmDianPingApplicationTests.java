be1114af4c3e544a4ca89dbf207e52aaf9a3857c
Litway_ <xmw0517@gmail.com>
2022-08-23 15:44:58 +0800

    hmdp|商户信息

--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,13 +1,23 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.CacheClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.constant.ShopConstant.CACHE_SHOP_KEY;
+
+
 @SpringBootTest
 class HmDianPingApplicationTests {
+
+    @Resource
+    private CacheClient cacheClient;
 
     @Resource
     private ShopServiceImpl shopService;
@@ -16,6 +26,13 @@ class HmDianPingApplicationTests {
     @Test
     void testSave() throws InterruptedException {
         shopService.saveShop2Redis(1L, 10L);
+    }
+
+    @Test
+    public void testCache() {
+        Shop shop = shopService.getById(1L);
+
+        cacheClient.setWithLogicalExpire(CACHE_SHOP_KEY + 1L, shop, 10L, TimeUnit.SECONDS);
     }
 
 
