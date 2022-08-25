fc1906198b067aac62c3a3c8f04c7d6aea54bd4c
Litway_ <xmw0517@gmail.com>
2022-08-25 23:09:24 +0800

    hmdp|商户信息  使用Executors.newFixedThread开启一个线程池500个线程 每个任务为生成100个id 通过500个线程将任务提交300次  共生成30k个id  测试的时候如果需要注入组件别忘了加上@SpringBootTest注解

--- hm-dianping/src/test/java/com/hmdp/utils/RedisIdToolTest.java
+++ hm-dianping/src/test/java/com/hmdp/utils/RedisIdToolTest.java
@@ -1,15 +1,50 @@
 package com.hmdp.utils;
 
-import org.junit.Test;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.junit.Assert.*;
 
+@SpringBootTest
 public class RedisIdToolTest {
 
+    @Resource
+    private RedisIdTool redisIdTool;
+
     private static final long BEGIN_TIMESTAMP = 1640995200L;
+
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+
+    @Test
+    public void testId() throws InterruptedException {
+        // 300个线程 所以这里面填写300 这些线程会依次减少
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdTool.nextId("order");
+                System.out.println("id = " + id);
+            }
+            latch.countDown();
+        };
+
+        long begin = System.currentTimeMillis();
+        // 使用500个线程将该任务提交300次 单位任务内容为生成100个id
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
+    }
 
     @Test
     public void testCount() {
