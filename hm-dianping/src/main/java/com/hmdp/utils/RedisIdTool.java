19b7be58b4260ed9ca62aeaf15d0df5474956657
Litway_ <xmw0517@gmail.com>
2022-08-25 23:19:14 +0800

    hmdp|商户信息  全局id生成器 测试的时候有些代码有价值

--- hm-dianping/src/main/java/com/hmdp/utils/RedisIdTool.java
+++ hm-dianping/src/main/java/com/hmdp/utils/RedisIdTool.java
@@ -1,35 +1,48 @@
 package com.hmdp.utils;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
+
 @Component
 public class RedisIdTool {
 
+    /**
+     * 基础秒数
+     */
     private static final long BEGIN_TIMESTAMP = 1640995200L;
+
+    /**
+     * 序列号的位数 -> 如果以后的算法设计中序列号位数增加的话 就可以改这里
+     */
+    private static final int COUNT_BITS = 32;
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public RedisIdTool(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
 
     public long nextId(String keyPrefix) {
         // 1- 生成时间戳
-
+        LocalDateTime now = LocalDateTime.now();
+        long nowSeconds = now.toEpochSecond(ZoneOffset.UTC);
+        long timestamp = nowSeconds - BEGIN_TIMESTAMP;
 
         // 2- 生成序列号
+        /*
+            不能这样递增:
+                stringRedisTemplate.opsForValue().increment("Icr" + keyPrefix);
+                因为这样的话就表明, keyPrefix对应的业务都是使用的一个自增的id, 那么随着时间的漂移总会有超过redis上限2^64的时候
+                因此在key后面再加上一个日期
+         */
+        long count = stringRedisTemplate.opsForValue().increment("Icr" + keyPrefix);
 
-
-        // 3- 拼接
-
-        return 0L;
+        // 3- 拼接 -> 算法设计为 最高位为1(符号位)  紧接着31位为时间戳  后32为为序列号. 因此需要先将时间戳左移32位 然后再进行或运算
+        return timestamp << COUNT_BITS | count;
     }
-
-
-
-    public static void main(String[] args) {
-        // 获取一个初始时间对应的秒数
-        LocalDateTime time = LocalDateTime.of(2022, 1, 1, 0, 0);
-        long l = time.toEpochSecond(ZoneOffset.UTC);
-        System.out.println(l);
-    }
-
 
 }
