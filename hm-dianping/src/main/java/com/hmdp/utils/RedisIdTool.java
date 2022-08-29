18eb88f4b5214f13760ff904b011214144e53bec
Litway_ <xmw0517@gmail.com>
2022-08-29 17:17:41 +0800

    master  redis存储全局id的时候 需要加上一个时间段的限制

--- hm-dianping/src/main/java/com/hmdp/utils/RedisIdTool.java
+++ hm-dianping/src/main/java/com/hmdp/utils/RedisIdTool.java
@@ -5,27 +5,37 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+import static com.hmdp.utils.RedisConstants.DELIMITER;
+import static com.hmdp.utils.RedisConstants.INCREMENT;
 
 
 @Component
 public class RedisIdTool {
 
     /**
-     * 基础秒数
+     * 基础秒数 -> 因为算法的高32位为时间 但是以秒数为存储 所以这里要计算一个开始时间距离1970-1-1的秒数
      */
     private static final long BEGIN_TIMESTAMP = 1640995200L;
 
     /**
-     * 序列号的位数 -> 如果以后的算法设计中序列号位数增加的话 就可以改这里1
+     * 序列号的位数 -> 如果以后的算法设计中序列号位数增加的话 就可以改这里
      */
     private static final int COUNT_BITS = 32;
 
     private final StringRedisTemplate stringRedisTemplate;
 
+    // 这是通过构造方法的方式去初始化StringRedisTemplate
     public RedisIdTool(StringRedisTemplate stringRedisTemplate) {
         this.stringRedisTemplate = stringRedisTemplate;
     }
 
+    /**
+     * 生成唯一的id
+     * @param keyPrefix 业务前缀
+     * @return id
+     */
     public long nextId(String keyPrefix) {
         // 1- 生成时间戳
         LocalDateTime now = LocalDateTime.now();
@@ -36,10 +46,15 @@ public class RedisIdTool {
         /*
             不能这样递增:
                 stringRedisTemplate.opsForValue().increment("Icr" + keyPrefix);
-                因为这样的话就表明, keyPrefix对应的业务都是使用的一个自增的id, 那么随着时间的漂移总会有超过redis上限2^64的时候
-                因此在key后面再加上一个日期
+                因为这样的话就表明, keyPrefix对应的业务都是使用的一个自增的id, 那么随着时间的漂移总会有超过redis上限2^64的时候,
+                而且此处设计的算法其序列号仅有32位, 并不是64位. 2^32大约为42亿, 这个值还是很可能超过的.
+                方法: 在key后面再加上一个日期, 还能起到一个统计效果. 比如加上单位天, 则能够统计当日的订单量, 加上月份能够统计当月的订单量.
+                在redis中, 以冒号为分隔符之后redis会自动帮助我们分层, 便于观察.
+
+                下面提示可能会报出Null错误, 其实不会: 因为
          */
-        long count = stringRedisTemplate.opsForValue().increment("Icr" + keyPrefix);
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        long count = stringRedisTemplate.opsForValue().increment(INCREMENT + keyPrefix + DELIMITER + date);
 
         // 3- 拼接 -> 算法设计为 最高位为1(符号位)  紧接着31位为时间戳  后32为为序列号. 因此需要先将时间戳左移32位 然后再进行或运算
         return timestamp << COUNT_BITS | count;
