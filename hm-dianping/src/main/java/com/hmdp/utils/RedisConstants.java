3316eafd3834c4e624a6498143a710e2973bdc4f
Litway_ <xmw0517@gmail.com>
2022-08-29 17:15:02 +0800

    master  redis中的一些常量 以冒号进行分割redis的key时 redis会进行分层

--- hm-dianping/src/main/java/com/hmdp/utils/RedisConstants.java
+++ hm-dianping/src/main/java/com/hmdp/utils/RedisConstants.java
@@ -16,6 +16,12 @@ public class RedisConstants {
     public static final String LOCK_SHOP_KEY = "Lock:Shop:";
     public static final Long LOCK_SHOP_TTL = 10L;
 
+    // redis的分隔符
+    public static final String DELIMITER = ":";
+
+    // redis自增
+    public static final String INCREMENT = "Icr";
+
 
     public static final String SECKILL_STOCK_KEY = "seckill:stock:";
     public static final String BLOG_LIKED_KEY = "blog:liked:";
