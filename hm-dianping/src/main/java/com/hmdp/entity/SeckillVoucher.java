67e9ff46f364767c6fa9e2c5509fbb7c0a6ef3bb
Litway_ <xmw0517@gmail.com>
2022-09-02 21:18:17 +0800

    master  @Version是乐观锁注解 支持的字段类型: long Long int Integer java.util.Date java.sql.Timestamp java.time.LocalDateTime

--- hm-dianping/src/main/java/com/hmdp/entity/SeckillVoucher.java
+++ hm-dianping/src/main/java/com/hmdp/entity/SeckillVoucher.java
@@ -3,6 +3,7 @@ package com.hmdp.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.Version;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -35,6 +36,7 @@ public class SeckillVoucher implements Serializable {
     /**
      * 库存
      */
+    @Version
     private Integer stock;
 
     /**
