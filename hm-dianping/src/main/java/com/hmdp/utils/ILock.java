a74708c5dacc50d41cc4306b6d1c4f6df25adb5b
Litway_ <xmw0517@gmail.com>
2023-02-14 11:12:46 +0800

    master  基于Redis分布式锁基础版本接口

---
+++ hm-dianping/src/main/java/com/hmdp/utils/ILock.java
@@ -0,0 +1,23 @@
+package com.hmdp.utils;
+
+/**
+ * ILock
+ *
+ * @author DELL_
+ * @date 2023/02/14
+ */
+public interface ILock {
+
+    /**
+     * 尝试获取锁
+     *
+     * @param timeoutSec 持有锁的时间, 超时自动释放锁, 单位秒
+     * @return true代表锁获取成功; false代表锁获取失败
+     */
+    boolean tryLock(long timeoutSec);
+
+    /**
+     * 释放锁
+     */
+    void unLock();
+}
