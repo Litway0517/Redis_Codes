0ff2f2a660ce2625874ceaaad58b0d23befb3922
Litway_ <xmw0517@gmail.com>
2023-02-18 10:47:33 +0800

    master  Redisson配置类

---
+++ hm-dianping/src/main/java/com/hmdp/config/RedissonConfig.java
@@ -0,0 +1,24 @@
+package com.hmdp.config;
+
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// 加上配置类注解, 将其加载到Spring容器
+@Configuration
+public class RedissonConfig {
+
+    // RedissonClient是一个工厂类, 从中就能取到各种工具类
+    @Bean
+    public RedissonClient redisClient() {
+        // 配置
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://169.254.118.150:6379").setPassword("");
+
+        // 创建RedissonClient对象
+        return Redisson.create(config);
+    }
+}
