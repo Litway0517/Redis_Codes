7bbe477042da8092e1baef94daaaddecdd30677c
Litway_ <xmw0517@gmail.com>
2023-02-20 22:01:50 +0800

    master  Redisson释放锁Lua脚本

--- hm-dianping/src/test/java/com/hmdp/utils/RedisIdToolTest.java
+++ hm-dianping/src/test/java/com/hmdp/utils/RedisIdToolTest.java
@@ -82,5 +82,23 @@ public class RedisIdToolTest {
 
     }
 
+    @Test
+    public void unlockRedisson() {
+        String redissonUnlockScript = "if (redis.call('hexists', KEYS[1], ARGV[3]) == 0) then " +
+                "return nil;" +
+                "end; " +
+                "local counter = redis.call('hincrby', KEYS[1], ARGV[3], -1); " +
+                "if (counter > 0) then " +
+                "redis.call('pexpire', KEYS[1], ARGV[2]); " +
+                "return 0; " +
+                "else " +
+                "redis.call('del', KEYS[1]); " +
+                "redis.call('publish', KEYS[2], ARGV[1]); " +
+                "return 1; " +
+                "end; " +
+                "return nil;";
+        System.out.println(redissonUnlockScript);
+    }
+
 
 }
