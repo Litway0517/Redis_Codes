8fc40559f59321a03635e1834dccfd8402241540
Litway_ <xmw0517@gmail.com>
2023-02-20 11:40:56 +0800

    master  Redisson的可重入锁的Lua脚本内容 更新注释

--- hm-dianping/src/main/resources/Redisson_1.lua
+++ hm-dianping/src/main/resources/Redisson_1.lua
@@ -3,7 +3,7 @@
 
 -- 如果锁不存在
 if (redis.call('exists', KEYS[1]) == 0) then
-    -- 将key对应的线程id计数+1
+    -- 将key对应的线程id计数+1(使用指令hincrby即使在一个key不存在的情况下使其+1)
     redis.call('hincrby', KEYS[1], ARGV[2], 1);
     -- 设置key的超时时间未ARGV[1]
     redis.call('pexpire', KEYS[1], ARGV[1]);
@@ -20,4 +20,6 @@ if (redis.call('hexists', KEYS[1], ARGV[2]) == 1) then
     -- 返回nil
     return nil;
 end ;
+
+-- key对应的锁存在 并且不是当前现成的
 return redis.call('pttl', KEYS[1]);
